96331fc5e722d6b859fc70fcdc87d77267d00b67
Alden0012 <ajs19@imperial.ac.uk>
2020-05-22 21:16:27 +0100

    added 34 opcodes, had to reclone this cause of some corrupt file in the OG directory, hopefully nothing bad happened lol

--- gg-gb-libs/CPU.cpp
+++ gg-gb-libs/CPU.cpp
@@ -81,17 +81,17 @@ unsigned CPU::tick(){
 			pc += 1;
 			break;
 		case 0x04:// INC B
-			UbAdd(&bc,1);
+			UbAdd(&bc,1,0);
 			cycles += 4;
 			pc += 1;
 			break;
 		case 0x14:// INC D
-			UbAdd(&de,1);
+			UbAdd(&de,1,0);
 			cycles += 4;
 			pc += 1;
 			break;
 		case 0x24:// INC H
-			UbAdd(&hl,1);
+			UbAdd(&hl,1,0);
 			cycles += 4;
 			pc += 1;
 			break;
@@ -110,25 +110,107 @@ unsigned CPU::tick(){
 			break;
 		}
 		case 0x0C://INC C
-			LbAdd(&bc,1);
+			LbAdd(&bc,1,0);
 			cycles += 4;
 			pc += 1;
 			break;
 		case 0x1C://INC E
-			LbAdd(&de,1);
+			LbAdd(&de,1,0);
 			cycles += 4;
 			pc += 1;
 			break;
 		case 0x2C://INC L
-			LbAdd(&hl);
+			LbAdd(&hl,1,0);
 			cycles += 4;
 			pc += 1;
 			break;
 		case 0x3C://INC A
-			UbAdd(&af);
+			UbAdd(&af,1,0);
 			cycles += 4;
 			pc += 1;
 			break;
+		case 0x0D://DEC C
+			LbSub(&bc,1,0);
+			cycles += 4;
+			pc += 1;
+			break;
+		case 0x1D://DEC E
+			LbSub(&de,1,0);
+			cycles += 4;
+			pc += 1;
+			break;
+		case 0x2D://DEC L
+			LbSub(&hl,1,0);
+			cycles += 4;
+			pc += 1;
+			break;
+		case 0x3D://DEC A
+			UbSub(&af,1,0);
+			cycles += 4;
+			pc += 1;
+			break;
+		case 0x05://DEC B
+			UbSub(&bc,1,0);
+			cycles += 4;
+			pc += 1;
+			break;
+		case 0x15://DEC D
+			UbSub(&de,1,0);
+			cycles += 4;
+			pc += 1;
+			break;
+		case 0x25://DEC H
+			UbSub(&hl,1,0);
+			cycles += 4;
+			pc += 1;
+			break;
+		case 0x35://DEC (HL)
+			{
+			uint8_t iByte = mmu.read(hl);
+			set_flag_hcarry(1 > iByte & 0x0F);
+			set_flag_carry(iByte < 1);
+			set_flag_subtract(0);
+			iByte++;
+			set_flag_zero((bool)iByte);
+
+			mmu.write(hl,iByte);
+			cycles += 12;
+			pc += 1;
+			break;
+		}
+		case 0x09:// Hl += BC
+			AddtoHL(&bc);
+			cycles += 8;
+			pc += 1;
+			break;
+		case 0x19:// HL += DE
+			AddtoHL(&de);
+			cycles += 8;
+			pc += 1;
+			break;
+		case 0x29:// HL += HL
+			AddtoHL(&hl);
+			cycles += 8;
+			pc += 1;
+			break;
+		case 0x39:// HL += SP
+			AddtoHL(&sp);
+			cycles += 8;
+			pc +=1;
+			break;
+		case 0x08://write 16-bit sp to address given in operand
+			addr = mmu.read16(pc+1);
+			mmu.write(addr+1,sp >> 8);
+			mmu.write(addr,sp & 0x00FF);
+			cycles += 20;
+			pc += 3;
+		
+
+
+
+
+
+
 
 
 	}
--- gg-gb-libs/CPUOperations.cpp
+++ gg-gb-libs/CPUOperations.cpp
@@ -15,7 +15,7 @@ void CPU::set_flag_carry(bool set){
 	af &= 0xFFEF;
 	af |= (set << 4);
 }
-void CPU::UbAdd(uint16_t *reg x,uint8_t n,bool carry){
+void CPU::UbAdd(uint16_t & x,const uint8_t n,const bool carry){
 	uint8_t ub = (x >> 8);
 	
 	if(n != 1){
@@ -33,7 +33,44 @@ void CPU::UbAdd(uint16_t *reg x,uint8_t n,bool carry){
 	}else{set_flag_zero(0);}
 	set_flag_subtract(0);
 }
-void CPU::LbAdd(uint16_t *reg x,uint8_t n,bool carry){
+void CPU::UbSub(uint16_t & x,const uint8_t n,const bool carry){
+	uint8_t ub = (x >> 8);
+	
+	if(n != 1){
+		set_flag_carry(ub < (carry + n));
+		set_flag_hcarry((n & 0x0F + (int)carry) > ub & 0x0F );
+	}else{
+		set_flag_hcarry((n & 0x0F + (int)carry) > ub & 0x0F );
+	}
+	ub -= n + (int)carry;
+	x = (0x00FF & x) + (ub << 8);
+	
+	if (x == 0){
+		set_flag_zero(1);
+
+	}else{set_flag_zero(0);}
+	set_flag_subtract(1);
+}
+
+
+void CPU::LbSub(uint16_t & x,const uint8_t n,const bool carry){
+	lb = 0x00FF & x;
+	if(n != 1){
+		set_flag_carry(lb < n + carry);
+		set_flag_hcarry((lb & 0xf)<(n&0xf + carry));
+	}else{
+		set_flag_hcarry((lb & 0xf)<(n&0xf + carry));
+	}
+	lb -= n + (int)carry;
+	lb &= 0x00FF;
+	x = (0xFF00 & x) | lb;
+	if (x == 0){
+		set_flag_zero(1);
+
+	}else{set_flag_zero(0);}
+	set_flag_subtract(1);
+}
+void CPU::LbAdd(uint16_t &x,const uint8_t n,const bool carry){
 	lb = 0x00FF & x;
 	if(n != 1){
 		set_flag_carry((uint16_t)(lb + n + (int)carry)> 0xFF);
@@ -44,5 +81,20 @@ void CPU::LbAdd(uint16_t *reg x,uint8_t n,bool carry){
 	lb += n + (int)carry;
 	lb &= 0x00FF;
 	x = (0xFF00 & x) | lb;
-}
+	if (x == 0){
+		set_flag_zero(1);
 
+	}else{set_flag_zero(0);}
+	set_flag_subtract(0);
+}
+void CPU::AddtoHL(const uint16_t &reg){
+	if(hl > (0xFFFF - reg)){ 
+		set_flag_carry(1);
+	}else{set_flag_carry(0);}
+	if((hl & 0xfff) + (reg & 0xfff) > 0xfff){
+		set_flag_hcarry(1);
+	}else{set_flag_hcarry(0;)}
+	set_flag_subtract(0);
+	hl += reg;
+
+}
